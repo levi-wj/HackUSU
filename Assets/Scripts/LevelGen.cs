223bf5fd0d2cde4286e7df153fc91c09ff987292
Levi Jones <leviwill.j@gmail.com>
2022-03-19 11:16:15 -0600

    got player movement smoother

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class LevelGen : MonoBehaviour
 {
-    [SerializeField] GameObject ground;
 
     // Start is called before the first frame update
     void Start()
     {
-               
     }
 
     // Update is called once per frame
